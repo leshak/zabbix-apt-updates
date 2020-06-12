733f961feef307f0c3921b0fa3be4149efa79325
Alexey Kruglov <leshak@leshak.ru>
2020-06-12 14:52:07 +0300

    fix

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ chmod 0755 /etc/zabbix/scripts/cron-zabbix-apt-update.sh
 chown -R root. /etc/zabbix/scripts/cron-zabbix-apt-update.sh
 
 echo "Add to cron job"
-
+echo "4 */4    * * *   root    /etc/zabbix/scripts/cron-zabbix-apt-update.sh" >> /etc/crontab
+systemctl restart cron
 
 echo "Run once"
 /etc/zabbix/scripts/cron-zabbix-apt-update.sh
